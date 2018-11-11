0b33eaaa3a6ca58ef2daa4b8c4a4d507fc3f0746
Xingfu <fxing606@gmail.com>
2018-11-11 16:27:21 +0800

    Longest Increasing Sequence Done.

---
+++ LCS.py
@@ -0,0 +1,40 @@
+#
+
+# Item 1: Longest Increasing Subsequence Size (NlogN)
+# Solution One: N^2 brute force
+# Solution Two: Dynamic Programming
+# How to add or replace an element in the existing sequence. Each new smallest element in the array can be
+# a potential candidate to start new sequence,so we must maintain lists of increasing sequences and update.
+# 使用二分法查找新的A[i]在数组tailTable中的位置，有序数组查找使用二分查找
+def CeilIndex(A,l,r,key):
+
+    while (r-l>1):
+        m=l+(r-1)//2
+        if (A[m]>=key):
+            r=m
+        else:
+            l=m
+    return r
+
+def LongestIncreasingSubsequenceLength(A,size):
+    tailTable=[0 for i in range(size+1)] # 使用任意元素初始化List，不用Numpy
+    len=0
+
+    tailTable[0]=A[0]
+    len=1
+    for i in range(1,size):
+        if (A[i]<tailTable[0]):
+            tailTable[0]=A[i]
+        elif (A[i]>tailTable[len-1]):
+            tailTable[len]=A[i]
+            len+=1
+        else:
+            tailTable[CeilIndex(tailTable,-1,len-1,A[i])]=A[i]
+
+    return len
+
+if __name__ == '__main__':
+    A=[2,5,3,7,11,8,10,13,6]
+    n=len(A)
+
+    print("Length of Longest Increasing Subsequence is ",LongestIncreasingSubsequenceLength(A,n))
