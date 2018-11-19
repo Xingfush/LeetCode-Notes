61061dea8b3003a44278486396253ecf05810fbd
Xingfu <fxing606@gmail.com>
2018-11-19 23:39:34 +0800

    two equal subsets partition added.

--- LCS.py
+++ LCS.py
@@ -133,13 +133,34 @@ def lower_bound(array,value):
 
     return -1
 
+# Problem 4: Partition equal subset sum
+# accessibility代表该元素是否可以由nums的子集组成，第一遍首先遍历num元素，显然每个元素都应该为True
+# 其次，i从half递减至num，每个i=num_now+num_before符合的都是True。这里最关键的是before的记录，
+# 正是dynamic programming的关键所在。
+def canPartition(nums):
+    Sum=sum(nums)
+    if Sum % 2!=0:
+        return False
+    half=Sum//2
+    accessibility=[False]*(half+1)
+    accessibility[0]=True
+    for num in nums:
+        if half>=num:
+            for i in range(half,num-1,-1):
+                if accessibility[i-num]==True:
+                    accessibility[i]=True
+
+    return accessibility[half]
+
 
 if __name__ == '__main__':
     X='saturdaywendy'
     Y='sundayandy'
     A=[1, 3, 5, 7 ,9 ,11 ,13, 15, 17, 19, 20]
     B=[2,5]
+    C=[1,2,3,5,6,5,2,1]
     #print("Length of Longest Common Subsequence is: ",LongestIncreaseSequence(A))
     #PrintLCS(X,Y,LongestCommonSequence(X,Y))
     #print("The key is in a region:",lower_bound(B,5))
-    print("the edit distance between str1 and str2 is:",editDistance(X,Y))
+    #print("the edit distance between str1 and str2 is:",editDistance(X,Y))
+    print("The array can partition into two subset with equal sum:", canPartition(C))
