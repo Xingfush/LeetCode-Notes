fdfeb82ab39656409ebab6cc1132832209cea891
Xingfu <fxing606@gmail.com>
2019-01-25 21:57:47 +0800

    unique subsequence version 1

---
+++ leetcode/LongestUniqueSubsequence.py
@@ -0,0 +1,16 @@
+def LongestUniqueSubsequence(s):
+    dic = {} # 只需要判断是否已存在（重复），不计数（不用Counter）
+    res = 0
+    j = 0
+    for i in range(len(s)):
+        if s[i] not in dic:
+            dic[s[i]] = 1 # 可以进一步简化为集合set
+        else:
+            while (s[i] in dic): # 更新游标的位置
+                dic.pop(s[j])
+                j += 1
+        res = max(res, i-j+1)
+    return res
+
+if __name__=='__main__':
+    print(LongestUniqueSubsequence("abcdefdhiajq"))
