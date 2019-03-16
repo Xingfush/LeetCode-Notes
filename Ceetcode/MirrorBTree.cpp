8ffc237df8dcde1595721a1fe98a1fe467d27203
Xingfu <fxing606@gmail.com>
2019-03-16 22:45:13 +0800

    add iterative traverse of BST.

--- Ceetcode/MirrorBTree.cpp
+++ Ceetcode/MirrorBTree.cpp
@@ -1,24 +1,57 @@
-void MirroPreBFS(TreeNode* root)
+
+/* 使用栈完成中序遍历 */
+
+void MirroInBFS(TreeNode* root)
 {
 	if(root==nullptr)
 		return;
 
 	stack<TreeNode *> nstack;
-	nstack.push(root);
+	TreeNode *node = root;
+
+	while(node!=nullptr ||!nstack.empty())
+	{
+		while(node!=nullptr)
+		{
+			nstack.push(node);
+			node=node->left;
+		}
+
+		if(!nstack.empty())
+		{
+			node = nstack.top();
+
+			if(node->left!=nullptr || node->right!=nullptr)
+				std::swap(node->left,node->right);
+
+			nstack.pop();
+			node=node->left; // 本来是 right，但是发生了交换
+		}
+	}
+}
+
+/* 使用队列完成层序遍历 */
+void MirroLayerBFS(TreeNode* root)
+{
+	if(root==nullptr)
+		return;
+
+	queue<TreeNode *> nqueue;
+	nqueue.push(root);
 
 	TreeNode *node =root;
-	while(!nstack.empty())
+	while(!nqueue.empty())
 	{
-		node = nstack.top();
-		nstack.pop();
+		node = nqueue.front();
+		nqueue.pop();
 
 		if(node->left!=nullptr || node->right!=nullptr)
 			std::swap(node->left,node->right);
 
 		if(node->left!=nullptr)
-			nstack.push(node->left);
+			nqueue.push(node->left);
 
 		if(node->right!=nullptr)
-			nstack.push(node->right);
+			nqueue.push(node->right);
 	}
 }
