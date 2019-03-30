dbab7095b7f1f77a55eea309d9d5bf3e74d70555
Xingfu <fxing606@gmail.com>
2019-03-30 09:07:08 +0800

    add pre-post order construction

---
+++ Ceetcode/ConstructTreePrePostOrder.cpp
@@ -0,0 +1,22 @@
+TreeNode* constructFromPrePost(vector<int> pre, vector<int> post) {
+    vector<TreeNode*> s;
+    s.push_back(new TreeNode(pre[0]));
+    for (int i = 1, j = 0; i < pre.size(); ++i) {
+        TreeNode* node = new TreeNode(pre[i]);
+        while (s.back()->val == post[j])
+            s.pop_back(), j++;
+        if (s.back()->left == NULL) s.back()->left = node;
+        else s.back()->right = node;
+        s.push_back(node);
+    }
+    return s[0];
+}
+
+// 简直是神作！！！
+/* Here I share my iterative solution.
+We will preorder generate TreeNodes, push them to stack and postorder pop them out.
+
+Loop on pre array and construct node one by one.
+stack save the current path of tree.
+node = new TreeNode(pre[i]), if not left child, add node to the left. otherwise add it to the right.
+If we meet a same value in the pre and post, it means we complete the construction for current subtree. We pop it from stack.*/
