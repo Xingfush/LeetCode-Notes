ffe548095fca80f89ffa9e2321ca9c88ac0e1ee9
Xingfu <fxing606@gmail.com>
2019-04-07 13:46:11 +0800

    add interview files

--- Ceetcode/二叉树/MaxWidth.cpp
+++ Ceetcode/二叉树/MaxWidth.cpp
@@ -1,24 +1,20 @@
-class Solution {
-private:
-    int maxVal = 0;
-    unordered_map<long,long> levelLeft;
-public:
-    int widthOfBinaryTree(TreeNode* root) {
-        dfs(root, 0, 0);
-        return maxVal;
-    }
+int widthOfBinaryTree(TreeNode* root){
+    int res = 0;
+    vector<int> start;
+    dfs(root,0,1,start,res); // 注意这里根节点是 1 索引的，只是标记作用，并没有实际的数组存储
+    return res;
+}
 
-    void dfs(TreeNode* root, long depth, long pos) 
-    {
-        if (root == NULL) 
-            return;
-        
-        if (levelLeft.find(depth)==levelLeft.end())
-           levelLeft[depth]=pos;
-        
-        maxVal = max(maxVal, pos - levelLeft[depth] + 1);
-        
-        dfs(root->left, depth + 1, 2 * pos); //make sure all left explored first using dfs
-        dfs(root->right, depth + 1, 2 * pos + 1);
-    }
-};
+void dfs(TreeNode* node, int h, int idx, vector<int>& start, int & res){
+    if(!node) return;
+    if(h>=start.size()) start.push_back(idx); // 
+    res = max(res, idx - start[h]+1);
+    dfs(node->left, h+1, 2*idx+1, start, res);
+    dfs(node->right, h+1, 2*idx+2, start, res);
+}
+
+/* 这道题充分利用了前序遍历的特点，前序遍历先访问左节点，所以每到新的一层，前序遍历访问的第一个结点
+    就是这一层的 leftest node，这样我们访问每一层的时候，直接使用 idx - start[h] +1 就可以得到
+    最大宽度。
+    1. 使用 h 来标记结点所属的层，用来访问 start[h];
+    2. idx 的计算使用 全局左边 2*i, 2*i+1 来转移到下一层的左右子结点。 */
