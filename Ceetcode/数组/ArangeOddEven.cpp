6b233b65fc368a351c52f13429fa330835e81a73
xingfu <fxing606@gmail.com>
2019-10-07 15:16:40 +0800

    rearrange the files

--- Ceetcode/数组/ArangeOddEven.cpp
+++ Ceetcode/数组/ArangeOddEven.cpp
@@ -1,13 +1,14 @@
-/* b站之耻，永世不忘 */
+/* 有一列数组，其中一般为奇数，一般为偶数，
+调整位置，要求奇数位置全为奇数，偶数位置全为偶数，
+常数空间，线性时间复杂度 */
 
-void reorderArray(vector<int>& nums)
-{
-	int even = 0, odd = 1, n = nums.size();
-	while (even<n && odd<n) {
-		while (nums[even] & 1) even += 2;
-		while ((nums[odd] & 1)==0) odd += 2;
-		swap(nums[even], nums[odd]);
-		even += 2;
-		odd += 2;
-	}
+void arrange_odd_even_elements(vector<int>& nums){
+    int odd = 0, even = 1; // 双标指针交换法
+    while(even<nums.size() && odd<nums.size()){
+        while(nums[odd] & 1) odd+=2;
+        while((nums[even] & 1) == 0) even+=2;
+        swap(nums[odd], nums[even]);
+        odd+=2;
+        even+=2;
+    }
 }
--- Ceetcode/数组/CountPrimes.cpp
+++ Ceetcode/数组/CountPrimes.cpp
@@ -1,15 +1,16 @@
-/* 方法很巧妙，要记住 */
+/* 求[1,n)范围内的素数个数 */
 
-int countPrimes(int n) {
+int count_primes(int n){
+    vector<bool> isPrime(n,true);
     int res = 0;
-    vector<bool> prime(n, true);
-    for (int i = 2; i < n; ++i) {
-        if (prime[i]) {
-            ++res;
-            for (int j = 2; i * j < n; ++j) {
-                prime[i * j] = false;
+    for(int i=2;i<n;i++){
+        if(isPrime[i]){
+            res++;
+            for(int j=2;i*j<n;j++){
+                isPrime[i*j]=false;
             }
         }
     }
-    return res;
+
+    return res++;
 }
